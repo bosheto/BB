8510cf505a7d3eb1454e93bbb7ee724ff6bd995c
bosheto <bgezkovski@gmail.com>
2020-11-08 03:11:04 +0200

    More indicators
    Added EMA indicator still needs some work

--- indicators.py
+++ indicators.py
@@ -1,6 +1,6 @@
 import pandas
 
-def MovingAverage(data_set, scale, color):
+def MA(data_set, scale, color):
     moving_avg = data_set['Close'].rolling(window=scale).mean()
     return {
         'data': moving_avg,
@@ -8,3 +8,12 @@ def MovingAverage(data_set, scale, color):
         'color': color
     }
 
+def EMA(data_set, scale, color):
+    dat = data_set.reindex(index=data_set.index[::-1])
+    eMa = dat['Close'].ewm(span=scale, min_periods=0, adjust=False, ignore_na=False).mean()
+    eMa = eMa.reindex(index=eMa.index[::-1])
+    return {
+        'data': eMa,
+        'scale': scale,
+        'color': color
+    }
--- ploter.py
+++ ploter.py
@@ -3,12 +3,14 @@ import plotly.graph_objs as go
 from datetime import datetime
 from dataB import get_binance_candle_data
 from binance.client import Client
-from indicators import MovingAverage
+from indicators import MA, EMA
 
 data = get_binance_candle_data('LINKUSDT', Client.KLINE_INTERVAL_30MINUTE)
 
-rolling_mean = MovingAverage(data, 7, 'green')
-rolling_mean_long = MovingAverage(data, 25, 'orange')
+rolling_mean = MA(data, 7, 'green')
+rolling_mean_long = MA(data, 25, 'orange')
+exponential_MA = EMA(data, 7, 'cyan')
+exponetial_MA_long = EMA(data, 25, 'orange')
 
 trace = go.Ohlc(
     x = data.index[:],
@@ -21,10 +23,13 @@ trace = go.Ohlc(
     decreasing=dict(line=dict(color='red')),
 )
 
-MA = go.Scatter(x=data.index[:], y=rolling_mean['data'], name='MA ({})'.format(rolling_mean['scale']), line=dict(color=rolling_mean['color']))
-MA1 = go.Scatter(x=data.index[:], y=rolling_mean_long['data'], name='MA ({})'.format(rolling_mean_long['scale']), line=dict(color=rolling_mean_long['color']))
+MA_chart = go.Scatter(x=data.index[:], y=rolling_mean['data'], name='MA ({})'.format(rolling_mean['scale']), line=dict(color=rolling_mean['color']))
+MA1_chart = go.Scatter(x=data.index[:], y=rolling_mean_long['data'], name='MA ({})'.format(rolling_mean_long['scale']), line=dict(color=rolling_mean_long['color']))
+exMA = go.Scatter(x=data.index[:], y=exponential_MA['data'], name= 'EMA ({})'.format(exponential_MA['scale'], line=dict(color=exponential_MA['color'])))
+exMA_long = go.Scatter(x=data.index[:], y=exponetial_MA_long['data'], name= 'EMA ({})'.format(exponetial_MA_long['scale'], line=dict(color=exponetial_MA_long['color'])))
 
-plot_data = [trace, MA, MA1]
+
+plot_data = [trace, MA_chart, MA1_chart, exMA, exMA_long]
 layout = {
     'title':'LINK USDT',
     'yaxis':{'title':'Price per coin'}
